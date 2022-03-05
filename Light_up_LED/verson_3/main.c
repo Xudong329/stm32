b4aed145294098a874799aa857bd83b22f382863
Xudong329 <95398335+Xudong329@users.noreply.github.com>
2022-03-04 20:17:48 -0400

    Create main.c

---
+++ Light_up_LED/verson_3/main.c
@@ -0,0 +1,30 @@
+#include "stm32f10x.h"
+
+
+int main(void)
+{	
+
+	RCC_APB2ENR|=( (1) << 3 );//RCC, IOPB enable
+	
+	//red light
+//	GPIOB->CRL |= ( (1) << (5*4) );//CRL, set IO as output with 10MHz push-pull
+//	GPIOB->ODR &= ~(1<<5);//ODR register enable(red light(PB5))
+	
+	//blue light
+//	GPIOB->CRL |= ( (1) << (1*4) );//CRL, set IO as output with 10MHz push-pull
+//	GPIOB->ODR	&= ~(1<<1);//ODR register enable(blue light(PB1))
+	
+	
+	//green light
+	GPIOB->CRL |= ( (1) << (0*4) );//CRL, set IO as output with 10MHz push-pull
+	GPIOB->ODR &= ~(1<<0);//ODR register enable(green light(PB0))
+}
+
+//set: |=(1<<n)
+//reset: &=~(1<<n)
+
+/*********************************************END OF FILE**********************/
+
+void SystemInit(void){//empty method for eliminate compile warning.
+
+}
